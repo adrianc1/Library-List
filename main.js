72e40425bf7563157d73154c874d75ea87806487
adriansito <adriancrosby02@gmail.com>
2024-06-10 20:08:04 -0700

    refactored this project using classes and IIFE

--- main.js
+++ main.js
@@ -13,45 +13,74 @@ let deleteBtn = document.querySelector('.fa-solid');
 let updateBtn = document.querySelector('.update-btn');
 let newBook = '';
 
-/* new book constructor */
-function Book(title, author, pages, isRead) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.isRead = isRead;
-}
 
-/* add book to library */
-function addBookToLibrary(book) {
-  myLibrary.push(book);
+class Book {
+  constructor(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+  }
+
+  addBook() {
+    myLibrary.push();
     tableBody.innerHTML += `
                 <tr> 
-                  <td class='book-title'>${book.title}</td>
-                  <td class='book-author'>${book.author}</td>
-                  <td class='book-pages'>${book.pages}</td>
-                  <td class='book-isRead'>${book.isRead}</td>
+                  <td class='book-title'>${this.title}</td>
+                  <td class='book-author'>${this.author}</td>
+                  <td class='book-pages'>${this.pages}</td>
+                  <td class='book-isRead'>${this.isRead}</td>
                   
                   <td class='delete'> <i class="fa-solid fa-trash"></i> </td>
                 </tr>`  
+  }
 }
 
-/* add inputs to new book */
-submitBtn.addEventListener('click', (event) => {
-  event.preventDefault();
-  let isReadOption = ''
 
-  for (let i =0; i < newIsRead.length; i++) {
-    if(newIsRead[i].checked) {
-      isReadOption = newIsRead[i].value;
+const eventHandlers = (() => {
+  submitBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    let isReadOption = ''
+  
+    for (let i =0; i < newIsRead.length; i++) {
+      if(newIsRead[i].checked) {
+        isReadOption = newIsRead[i].value;
+      }
     }
+    let book1 = new Book(newTitle.value, newAuthor.value, newPages.value, isReadOption );
+    book1.addBook(book1);
+    newTitle.value = '';
+    newAuthor.value = '';
+    newPages.value = ''
+    newIsRead.value = ''
+  })
+
+  // show form button
+  showFormBtn.addEventListener('click', showForm);
+
+  /* update read status */
+  tableBody.addEventListener('click', (event) => {
+    if(!event.target.classList.contains('book-isRead')) {
+      return
+    } 
+      if(event.target.closest('.book-isRead').textContent === 'no') {
+        event.target.closest('.book-isRead').textContent = 'yes'
+      } else {
+        event.target.closest('.book-isRead').textContent = 'no'
+      }
+  })
+
+  /* delete book record */
+tableBody.addEventListener('click', (event) => {
+  if (!event.target.classList.contains('fa-solid')) {
+    return
   }
-  let book1 = new Book(newTitle.value, newAuthor.value, newPages.value, isReadOption );
-  addBookToLibrary(book1);
-  newTitle.value = '';
-  newAuthor.value = '';
-  newPages.value = ''
-  newIsRead.value = ''
-} )
+  const removeBtn = event.target;
+  removeBtn.closest('tr').remove();
+}
+  );
+  
+})();
 
 
 /* show or hide form */
@@ -66,28 +95,8 @@ submitBtn.addEventListener('click', (event) => {
   }
 } 
 
-/* update read status */
-tableBody.addEventListener('click', (event) => {
-  if(!event.target.classList.contains('book-isRead')) {
-    return
-  } 
-    if(event.target.closest('.book-isRead').textContent === 'no') {
-      event.target.closest('.book-isRead').textContent = 'yes'
-    } else {
-      event.target.closest('.book-isRead').textContent = 'no'
-    }
-})
 
 
-/* delete book record */
 
-tableBody.addEventListener('click', (event) => {
-  if (!event.target.classList.contains('fa-solid')) {
-    return
-  }
-  const removeBtn = event.target;
-  removeBtn.closest('tr').remove();
-}
-  );
-  
+
 
